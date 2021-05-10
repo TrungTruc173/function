84febda0f68505ad40fe20cb59f233bf6b914ce8
TrungTruc173 <trungtruc9a2@gmail.com>
2021-05-10 12:41:51 +0700

    bai tap

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
-public class Main {
+       public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("function 2");
-    }
-}
+            public static void main(String[] args) {
+                new Thread(()->{
+                    for (int i =0; i< 10; i++) {
+                        System.out.println("a");
+                        try{
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }).start();
+                //thread 1
+
+                //thread2
+                for (int i =0; i< 10; i++) {
+                    System.out.println("b");
+                    try{
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
